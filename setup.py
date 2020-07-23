afe746e7b87ee2549c4f6a0942fa5f1b06f5b882
Kalyan Dutia <kalyan.dutia@gmail.com>
2020-07-23 17:28:10 +0100

    backbone

---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="elastic-wikidata",
+    version="0.0.1",
+    author="Science Musuem Group",
+    description="elastic-wikidata",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/TheScienceMuseum/elastic-wikidata",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+    packages=setuptools.find_packages(),
+)
