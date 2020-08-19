eef9e9daf106c072eed3690e69b52db6b455ead3
Kalyan Dutia <kalyan.dutia@gmail.com>
2020-08-19 16:56:13 +0100

    bump version 0.2.0 -> 0.2.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="elastic-wikidata",
-    version="0.2.0",
+    version="0.2.1",
     author="Science Musuem Group",
     description="elastic-wikidata",
     long_description=long_description,
