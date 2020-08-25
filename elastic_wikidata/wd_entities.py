510a97ec1e930ed274ee0a0c6cf768f5ab9532eb
Kalyan Dutia <kalyan.dutia@gmail.com>
2020-08-25 16:29:38 +0100

    wbgetentities timeout

--- elastic_wikidata/wd_entities.py
+++ elastic_wikidata/wd_entities.py
@@ -36,7 +36,7 @@ class get_entities:
         return "%7C".join(params) if len(params) > 1 else params[0]
 
     @classmethod
-    def get_all_results(self, qcodes, lang="en", page_limit=50) -> list:
+    def get_all_results(self, qcodes, lang="en", page_limit=50, timeout=3) -> list:
         """
         Get response through the `wbgetentities` API. 
 
@@ -44,7 +44,7 @@ class get_entities:
             list: each item is a the response for an entity
         """
 
-        results = self().result_generator(qcodes, lang, page_limit)
+        results = self().result_generator(qcodes, lang, page_limit, timeout)
 
         all_results = []
 
@@ -56,7 +56,7 @@ class get_entities:
         return all_results
 
     @classmethod
-    def result_generator(self, qcodes, lang="en", page_limit=50) -> list:
+    def result_generator(self, qcodes, lang="en", page_limit=50, timeout=3) -> list:
         """
         Get response through the `wbgetentities` API. Yields `page_limit` entities at a time.
 
@@ -71,7 +71,11 @@ class get_entities:
             qcodes[i : i + page_limit] for i in range(0, len(qcodes), page_limit)
         ]
 
+        headers = {
+            "User-Agent": "Elastic Wikidata/0.1",
+        }
+
         for page in qcodes_paginated:
             url = f"http://www.wikidata.org/w/api.php?action=wbgetentities&format=json&ids={self._param_join(page)}&props={self._param_join(self().properties)}&languages={lang}&languagefallback=1&formatversion=2"
-            response = requests.get(url).json()
+            response = requests.get(url, headers=headers, timeout=timeout).json()
             yield [v for _, v in response["entities"].items()]
