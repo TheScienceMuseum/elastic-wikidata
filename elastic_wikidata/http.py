c6948a32da0e268aa80036f2184296a0e41f8938
Kalyan Dutia <kalyan.dutia@gmail.com>
2020-08-26 11:14:20 +0100

    process contact information in user-agent header

--- elastic_wikidata/http.py
+++ elastic_wikidata/http.py
@@ -1,5 +1,6 @@
 import requests
 import sys
+from urllib.parse import quote
 from elastic_wikidata import __version__ as ew_version
 
 
@@ -17,9 +18,41 @@ def generate_user_agent(contact_information: str = None):
     }
 
     if contact_information is not None:
+        contact_information = " ".join(
+            [process_user_agent_username(i) for i in contact_information.split(" ")]
+        )
         return f"{v_params['ew']} ({contact_information}) {v_params['http_backend']} {v_params['python']}"
     else:
         print(
             "WARNING: please consider adding contact information to config.ini to improve the User Agent header for Wikidata requests."
         )
         return f"{v_params['ew']} {v_params['http_backend']} {v_params['python']}"
+
+
+def process_user_agent_username(username=None):
+    """
+    **Credit to [pywikibot](https://www.mediawiki.org/wiki/Manual:Pywikibot)**
+
+    Reduce username to a representation permitted in HTTP headers.
+
+    To achieve that, this function:
+    1) replaces spaces (' ') with '_'
+    2) encodes the username as 'utf-8' and if the username is not ASCII
+    3) URL encodes the username if it is not ASCII, or contains '%'
+    """
+    if not username:
+        return ""
+
+    username = username.replace(" ", "_")  # Avoid spaces or %20.
+    try:
+        username.encode("ascii")  # just test, but not actually use it
+    except UnicodeEncodeError:
+        username = quote(username.encode("utf-8"))
+    else:
+        # % is legal in the default $wgLegalTitleChars
+        # This is so that ops know the real pywikibot will not
+        # allow a useragent in the username to allow through a hand-coded
+        # percent-encoded value.
+        if "%" in username:
+            username = quote(username)
+    return username
